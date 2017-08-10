27a9bf42b8c740bed63a5fdfa9d3ded9202f6bbc
Hari Gopal <mail@harigopal.in>
2017-08-10 23:35:11 +0530

    Set up some simple centralization styling in-situ

--- index.js
+++ index.js
@@ -3,9 +3,14 @@ import ReactDOM from 'react-dom'
 import './src/style/index.scss'
 
 ReactDOM.render(
-  <div>
-    <h1>Hello, world!</h1>
-    <div className="m-1 turaku__container">This text is in an <code>m-1</code> <em>div</em>.</div>
+  <div className="container">
+    <div className="row justify-content-center" style={{height: '100vh', minHeight: '25rem'}}>
+      <div className="col-md-6 align-self-center">
+        <h1>Hello, world!</h1>
+        <br/>
+        <div className="m-1 turaku__container">This text is in an <code>m-1</code> <em>div</em>.</div>
+      </div>
+    </div>
   </div>,
   document.getElementById('turaku__root')
 )
